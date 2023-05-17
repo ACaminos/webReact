01e5f9f1e2330e7f614cf30e3a35c2953302e73d
Ariel Caminos <caminosariel@gmail.com>
2023-05-17 17:51:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { HeaderNav } from './components/HeaderNav'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <HeaderNav/>
+    </>
+  )
+}
+
+export default App
